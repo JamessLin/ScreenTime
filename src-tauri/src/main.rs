1105cd82524767968108933f1d6f1c26372a5745
james <jameslin809@gmail.com>
2025-03-05 22:45:31 -0500

    d

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,21 +9,6 @@ use tauri::{Manager, State};
 use app_usage::{get_app_usage, start_tracking};
 
 fn main() {
-    // let tracking_data = Arc::new(Mutex::new(HashMap::new()));
-    // println!("Starting application");  // Debug log
-    
-    // tauri::Builder::default()
-    //     .manage(AppState {
-    //         tracking: tracking_data.clone(),
-    //     })
-    //     .invoke_handler(tauri::generate_handler![get_tracking_data])
-    //     .setup(|app| {
-    //         println!("Setting up tracking thread");  // Debug log
-    //         tracking::start_tracking_thread(tracking_data);
-    //         Ok(())
-    //     })
-    //     .run(tauri::generate_context!())
-    //     .expect("error while running tauri application");
     tauri::Builder::default()
     .setup(|app| {
         start_tracking();
